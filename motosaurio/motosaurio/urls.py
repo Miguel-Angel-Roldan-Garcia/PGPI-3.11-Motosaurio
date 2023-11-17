36dd06a1d4584189a209913654a24e9cca6da319
Lucía Pérez <100713036+Lucperrom@users.noreply.github.com>
2023-11-17 11:11:37 +0000

    Login, Register, Logout

--- motosaurio/motosaurio/urls.py
+++ motosaurio/motosaurio/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("acount/", include("account.urls")),
+    path("account/", include("account.urls")),
 ]
