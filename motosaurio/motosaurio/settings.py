49bb3e1c32e8e7a894017114bc588ca7a0e8c02e
Lucía Pérez <100713036+Lucperrom@users.noreply.github.com>
2023-11-20 10:44:55 +0000

    Datos añadidos

--- motosaurio/motosaurio/settings.py
+++ motosaurio/motosaurio/settings.py
@@ -122,8 +122,8 @@ LOGIN_REDIRECT_URL = 'dashboard'
 LOGIN_URL = 'login'
 LOGOUT_URL = 'logout'
 
-MEDIA_URL = 'media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+# MEDIA_URL = 'media/'
+# MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
--- motosaurio/shop/views.py
+++ motosaurio/shop/views.py
@@ -6,11 +6,35 @@ from django.views.generic import TemplateView
 from .models import Product
 
 class ListProducts(TemplateView):
-    template_name = "product_list.html"
     
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
+    # def get_context_data(self, **kwargs):
+    #     context = super().get_context_data(**kwargs)
+    #     products = Product.objects.all()
+    #     context['products'] = products
+    #     context['section'] = 'dashboard'
+    #     return context
+    
+    def get(self, request, *args, **kwargs):
+        template_name = "product_list.html"
+        
+        name_query = request.GET.get('name', None)
+        fabricante = request.GET.get('producer', None)
+        tipo = request.GET.get('product_type', None)
+
         products = Product.objects.all()
+
+        if fabricante and tipo:
+            products = Product.objects.filter(producer = fabricante, product_type = tipo)
+        elif fabricante:
+            products = Product.objects.filter(producer = fabricante)
+        elif tipo:
+            products = Product.objects.filter(product_type = tipo)
+
+        if name_query:
+            products = [p for p in products if name_query in p.name]
+ 
+
+        context = dict()
         context['products'] = products
-        return context
-    
+        context['section'] = 'dashboard'
+        return render(request, template_name=template_name, context = context)
