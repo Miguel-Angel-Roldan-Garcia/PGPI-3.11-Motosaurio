4be97db51790c6f3fc6be23a3fb1617041975838
guigalde <guillermo.galeanodepaz@gmail.com>
2023-12-01 11:58:18 +0100

    Primera version de model

--- motosaurio/order/models.py
+++ motosaurio/order/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+class CartItem(models.model):
+    item = models.ForeignKey(Product, on_delete=models.CAS)
+    quantity = models.IntegerField()
+    order = models.ForeignKey(Order)
+
+class Order(models.model):
+    customer = models.ForeignKey(User, null=True)
+    order_id = models.IntegerField()
+
+class AnonimusOrder(models.model):
+    first_name = models.CharField('Nombre',max_length=200)
+    email = models.CharField('Correo elecrónico',max_length=200)
+    direccion = models.CharField('Dirección postal', max_length=200)
+    order_id = models.IntegerField()
