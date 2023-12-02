4c65a20eaea094eb6e9a6b444d7e3ae1d6b41732
alvurqmar <alvurqmar@alum.us.es>
2023-12-02 18:55:26 +0100

    Primera versión de envio de correo

--- motosaurio/cart/views.py
+++ motosaurio/cart/views.py
@@ -3,6 +3,9 @@ from django.views.decorators.http import require_POST
 from shop.models import Product
 from .cesta import Cesta
 from .forms import CestaAddProductForm
+from django.core.mail import send_mail
+from django.shortcuts import render, redirect
+from motosaurio.models import MiUsuario
 
 @require_POST
 def cesta_add(request, product_id):
@@ -30,3 +33,30 @@ def cesta_detail(request):
                                     'quantity': item['quantity'],
                                     'override': True})
  return render(request, 'cesta/detail.html', {'cesta': cesta})
+
+def send_confirmation_mail(id_pedido):
+    try:
+      #  order = Pedido.objects.get(id=id_pedido) Quitar cuando tengamos el pedido 
+        user = MiUsuario
+
+        userName = user.username
+        userMail = user.email
+
+        asunto = f'Confirmación de pedido #{id_pedido}'
+        mensaje = f'Hola {userName},\n\nGracias por realizar el pedido #{id_pedido} con nosotros!. ¡Estamos procesando tu solicitud y la enviaremos a la dirección proporcionada!\n\nGracias, El equipo de Motosaurio.'
+
+        sender_mail = 'motosaurio.project@outlook.com'
+
+        send_mail(
+            asunto,
+            mensaje,
+            sender_mail,
+            [userMail],
+            fail_silently=False,
+        )
+
+        return True  
+
+    except Exception as e:
+        print(f"Error al enviar el correo: {e}")
+        return False
