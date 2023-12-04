1a2a6410f395766a3e236adf626bd2c5d227e249
Lucía Pérez <100713036+Lucperrom@users.noreply.github.com>
2023-12-04 11:55:52 +0000

    Validación y código postal

--- motosaurio/motosaurio/models.py
+++ motosaurio/motosaurio/models.py
@@ -1,10 +1,13 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.core.validators import MaxValueValidator
 
 class MiUsuario(AbstractUser):
+
     first_name = models.CharField('Nombre',max_length=200)
     email = models.CharField('Correo elecrónico',max_length=200)
     direccion = models.CharField('Dirección postal', max_length=200)
+    codigo = models.IntegerField('Código postal',validators=[MaxValueValidator(99999)])
     tarjeta = models.BigIntegerField('Tarjeta de crédito', null=True)
 
     def __str__(self):
--- motosaurio/reclamaciones/urls.py
+++ motosaurio/reclamaciones/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
-app_name = 'cesta'
+app_name = 'reclamaciones'
 
 urlpatterns = [
  path('', views.register, name='register'),
